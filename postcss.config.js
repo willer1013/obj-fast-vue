cdffbe559fbccd0b22921d43143d0c0b61896082
KangKang0928 <443456429@qq.com>
2022-11-11 02:53:22 +0800

    folk from 5.2.0

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: chenkangxu
+ * @Date: 2022-05-30 11:02:50
+ * @LastEditTime: 2022-05-30 11:02:54
+ * @LastEditors: chenkangxu
+ * @Description: 
+ * @Github: 
+ */
+// postcss.config.js
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: chenkangxu
+ * @Date: 2022-05-30 11:02:06
+ * @LastEditTime: 2022-05-30 12:04:42
+ * @LastEditors: chenkangxu
+ * @Description: 
+ * @Github: 
+ */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
+}
